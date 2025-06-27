200c10103cf546e50a2e5d253c562ff0d1ca3f7b
Maurya <Raj.Maurya@motherson.com>
2025-06-27 10:42:17 +0530

    DOne

--- src/components/Homepage/footer.tsx
+++ src/components/Homepage/footer.tsx
@@ -14,13 +14,16 @@ const Footer = () => {
         <h2 className="text-2xl font-semibold">Engineers At AKTU</h2>
 
         {/* Social + Email */}
-        <div className="flex items-center justify-center space-x-2">
+        <div className="flex items-center justify-center space-x-3">
           <a
             href="https://www.linkedin.com/in/engineers-at-aktu-aa861a2b9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="LinkedIn"
-            className="hover:text-blue-500 transition-colors"
+            className="hover:text-blue-500 transition-colors transform
+    transition-transform
+    duration-200
+    hover:scale-110"
           >
             <LinkedinFilled style={{ fontSize: 24 }} />
           </a>
@@ -29,13 +32,16 @@ const Footer = () => {
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Instagram"
-            className="hover:text-red-500 transition-colors"
+            className="hover:text-red-500 transition-colors transform
+    transition-transform
+    duration-200
+    hover:scale-110"
           >
             <InstagramOutlined style={{ fontSize: 24 }} />
           </a>
           <a
             href="mailto:engineersataktu@gmail.com"
-            className="flex items-center space-x-2 hover:text-blue-500 transition-colors"
+            className="flex items-center space-x-2 hover:text-blue-500 transition-colors "
           >
             <span className='pl-1 border-l border-white'>engineersataktu@gmail.com</span>
           </a>
